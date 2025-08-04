bab73cb33b634495afa6f26f34ee638fffd2e060
Nguyễn Phúc Vinh <136446610+vinhngph@users.noreply.github.com>
2025-08-04 22:33:01 +0700

    fix: cookie issue

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, request, abort, jsonify, send_from_directory, render_template, make_response
+from flask import (
+    Flask,
+    request,
+    abort,
+    jsonify,
+    render_template,
+    make_response,
+    send_file,
+)
 from werkzeug.datastructures.file_storage import FileStorage
 import uuid
 import os
+from datetime import datetime, timedelta, timezone
 from utils.sql import SQL
 
 storage_dir = "storage"
@@ -21,8 +30,7 @@ def file():
             return jsonify({"msg": "invalid id"}), 400
 
         db = SQL(db_url)
-        file_data = db.execute(
-            "SELECT id, name, u_id FROM files WHERE id=?", (id,))
+        file_data = db.execute("SELECT id, name, u_id FROM files WHERE id=?", (id,))
 
         if not file_data or isinstance(file_data, int):
             return jsonify({"msg": "file not found"}), 404
@@ -30,12 +38,16 @@ def file():
         id: str = file_data[0]["id"]
         name: str = file_data[0]["name"]
 
-        return send_from_directory(storage_dir, id, as_attachment=False, download_name=name)
+        return send_file(
+            path_or_file=os.path.join(storage_dir, id),
+            as_attachment=True,
+            download_name=name,
+        )
 
     elif request.method == "POST":
-        user_id = request.cookies.get("user_id")
+        user_id = request.cookies.get("u_id")
         if not user_id:
-            return jsonify({"msg": "missing user_id"}), 400
+            return jsonify({"msg": "missing u_id"}), 400
 
         file: FileStorage = request.files["f"]
         if not file:
@@ -46,8 +58,10 @@ def file():
 
         try:
             db = SQL(db_url)
-            db.execute("INSERT INTO files (id, name, u_id) VALUES (?, ?, ?)",
-                       (file_id, file_name, user_id))
+            db.execute(
+                "INSERT INTO files (id, name, u_id) VALUES (?, ?, ?)",
+                (file_id, file_name, user_id),
+            )
             file.save(os.path.join("storage", file_id))
         except Exception as e:
             return jsonify({"msg": f"saving failed {e}"}), 500
@@ -61,7 +75,7 @@ def file():
 def home():
     db = SQL(db_url)
 
-    user_id = request.cookies.get("user_id")
+    user_id = request.cookies.get("u_id")
     files = db.execute("SELECT id, name FROM files WHERE u_id=?", (user_id,))
     response = make_response(render_template("home.html", files=files))
 
@@ -69,12 +83,15 @@ def home():
     if not user_id or not rows or isinstance(rows, int) or len(rows) != 1:
         user_id = str(uuid.uuid4())
         db.execute("INSERT INTO users (id) VALUES (?)", (user_id,))
+        expire_date = datetime.now(timezone.utc) + timedelta(days=365 * 5)
+
         response.set_cookie(
-            "user_id",
-            user_id,
+            key="u_id",
+            value=user_id,
             httponly=True,
             secure=True,
             samesite="Strict",
-            max_age=3600
+            max_age=157680000,
+            expires=expire_date,
         )
     return response
